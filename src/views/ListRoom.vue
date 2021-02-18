d35f2b85d5f91aca0607c26ea74e1ffdb617968f
Edwin Dharmawan <edwindharmawan@gmail.com>
2021-02-18 23:11:35 +0700

    add: user input name, listroom

---
+++ src/views/ListRoom.vue
@@ -0,0 +1,105 @@
+<template>
+  <section class="container">
+    <div class="row" id="header">
+      <div>
+        <h2>Pilih Room</h2>
+        <p>Game dimulai ketika salah satu player mengklik 'start'</p>
+      </div>
+      <h4>Halo {{username}}</h4>
+    </div>
+    <div id="cardList" class="row">
+      <div class="card">
+        <img class="card-img-top" src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Room 1</h5>
+          <p class="card-text">Room Name/ genre/ years</p>
+          <div class="button">
+            <a href="#" class="btn btn-primary">JOIN</a>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Room 2</h5>
+          <p class="card-text">Room Name/ genre/ years</p>
+          <div class="button">
+            <a href="#" class="btn btn-primary">JOIN</a>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Room 3</h5>
+          <p class="card-text">Room Name/ genre/ years</p>
+          <div class="button">
+            <a href="#" class="btn btn-primary">JOIN</a>
+          </div>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Room 4</h5>
+          <p class="card-text">Room Name/ genre/ years</p>
+          <div class="button">
+            <a href="#" class="btn btn-primary">JOIN</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ListRoom',
+  data () {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    getUser () {
+      this.username = localStorage.username
+    }
+  },
+  created () {
+    this.getUser()
+  }
+}
+</script>
+
+<style scoped>
+section{
+  height: 100vh;
+  margin-top: auto;
+  padding-top: 5vh;
+}
+#header{
+  justify-content: space-between;
+  align-items: center;
+}
+#header div{
+  text-align: left;
+  justify-content: flex-start;
+}
+#cardList{
+  display: flex;
+  justify-content: space-between;
+}
+.card{
+  margin-bottom: 5vh;
+  width: 40vh;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.card img{
+  height: 50vh;
+  width: 40vh;
+}
+h5{
+  text-align: center;
+}
+</style>
---
+++ src/views/UserStart.vue
@@ -0,0 +1,83 @@
+<template>
+  <section id="user">
+    <div id="div">
+      <img class="img" src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/06/music-logo-design.jpg">
+      <div id="items">
+        <h3 class="h1">TEBAK LAGU</h3>
+        <h6 class="h4">Selamat datang di <b>TEBAK LAGU</b> disini kamu ditantang untuk menebak beberapa lagu yang sudah disiapkan, bukan hanya itu saja loh disini kamu dapat bertanding melawan teman atau bahkan user baru loh</h6>
+      </div>
+    </div>
+    <div id="form">
+      <input id="inputUser" type="text" style="width: 60vh; height:7vh" v-model="username" placeholder="Input nama kamu disini"> <br>
+      <button @click="getUsername" class="btn btn-md btn-primary">START</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'UserStart',
+  data () {
+    return {
+      username: ''
+    }
+  },
+  methods: {
+    getUsername () {
+      localStorage.setItem('username', this.username)
+      this.$router.push({ name: 'ListRoom' })
+    }
+  }
+}
+</script>
+
+<style scoped>
+body{
+  margin: 0;
+  padding: 0;
+}
+#user {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100vh;
+  text-align: center;
+  color: #2c3e50;
+  padding: 0;
+  background-color: #ffb902;
+}
+.img{
+  width: 50vh;
+}
+.h1{
+  justify-items: center;
+  text-align: left;
+}
+.h4{
+  text-align: left;
+}
+#items{
+  align-items: center;
+  justify-items: center;
+  width: 60vh;
+}
+#div{
+  padding-top: 15vh;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+#form{
+  margin-top: 3vh;
+  margin: auto;
+  padding: 3vh;
+  width: 80vh;
+  border-radius: 25px;
+}
+#inputUser{
+  border-radius: 25px;
+  border-color: rgb(199, 196, 196);
+  margin-bottom: 2vh;
+  border-style: none;
+}
+</style>
