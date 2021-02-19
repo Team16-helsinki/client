9cbcb35e608a4331a80dcb0eadcc2a6675ca3cc5
kudaer69 <clvnyd96@gmail.com>
2021-02-19 13:16:24 +0700

    add default 404 error route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import UserStart from '../views/UserStart.vue'
 import ListRoom from '../views/ListRoom.vue'
 import PlayRoom from '../views/PlayRoom.vue'
@@ -10,19 +9,6 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/user',
     name: 'UserStart',
     component: UserStart
   },
@@ -35,6 +21,11 @@ const routes = [
     path: '/playroom',
     name: 'PLayRoom',
     component: PlayRoom
+  },
+  {
+    path: '*',
+    name: '404',
+    component: () => import('../views/404')
   }
 ]
 
